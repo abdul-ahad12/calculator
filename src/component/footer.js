b90085f486e6064f98460a232e144aa25840f141
SufyanKhan0 <67229679+SufyanKhan0@users.noreply.github.com>
2022-11-02 02:18:45 +0530

    footer color

--- src/component/footer.js
+++ src/component/footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="w-full bg-slate-800 text-white flex justify-center base:text-[10px] lg:text-[1rem] items-center h-9">
+    <div className="w-full bg-[#383838] text-white flex justify-center base:text-[10px] lg:text-[1rem] items-center h-9">
       {" "}
       Copyright© 2021 | Designed and Developed by AJK.{" "}
     </div>
