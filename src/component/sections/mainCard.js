5d231017a1f685ef2dedd2b5eaefbb3433efa1f2
abdul-ahad12 <abdulkouzer@gmail.com>
2022-11-04 02:43:26 +0530

    contact emal done

--- src/component/sections/mainCard.js
+++ src/component/sections/mainCard.js
@@ -6,7 +6,7 @@ const MainCard = ({title,link,url}) => {
     <div className=' min-w-[15rem]  max-w=[25rem] shadow-2xl rounded-b-[10px] bg-white flex flex-col '>
     <div className='h-60 [w-15rem] bg-white'><img className='w-full h-full' src={url} alt="img"></img></div>
     <div className='flex h-16 items-center justify-center '> 
-    <Link to={link}> <button  className='border-2 w-32 hover:bg-black hover:text-white border-black rounded-[1px]  text-black px-5 py-1'>{title}</button></Link>
+    <Link to={link}><button  className='border-2 w-32 hover:bg-black hover:text-white border-black rounded-[1px]  text-black px-5 py-1'>{title}</button></Link>
     </div>
 
     </div>
--- src/pages/contactus.js
+++ src/pages/contactus.js
@@ -1,24 +1,73 @@
+// import emailjs from '@emailjs/browser';
+import emailjs from "emailjs-com";
+
 import MainLayout from "./mainLayout";
 
 const Contactus = () => {
-  
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_owzhoi9",
+        "template_8zrb4m3",
+        e.target,
+        "ugaGXz6g1deIfQib2"
+      )
+      .then(
+        (result) => console.log(result.text),
+        (error) => console.log(error.text),
+        e.target.reset(),
+        alert("We will contact you soon")
+      );
+  };
 
   return (
     <div>
       <MainLayout>
         <div className="w-full flex  justify-center items-center ">
-          <div className="max-w-[40rem] bg-[#f6f6f6] shadow-2xl w-[35rem] min-w-[18rem] h-[35rem] items-center py-5 my-3 flex flex-col mx-4 rounded-[25px] gap-2">
+          <div className="max-w-[40rem] bg-[#f6f6f6] shadow-2xl w-[35rem] min-w-[18rem] min-h-[35rem] items-center py-5 md:my-10 base:my-4 flex flex-col mx-4 rounded-[25px] gap-2">
             <div className="text-[1.5rem] font-[600] ">Contact Us</div>
-            <div className="text-[1.2rem]">Any problem you have to add</div>
-            <form className="flex flex-col w-[90%] gap-2" action="submit">
+            <div className="text-[1.2rem] text-center">
+              Have feedback for us?
+              <br/>
+              Want us to add some new calculator<br/>
+              Found a bug?<br />
+              Just want to connect?<br/>
+              Feel free to use the following form,we<br />
+              respond to every message.
+            </div>
+            <form
+              className="flex flex-col w-[90%] gap-2"
+              onSubmit={sendEmail}
+              action="submit"
+            >
               <label className="font-[600]">Name</label>
-              <input className="p-2 bg-[#fbfbfb] rounded-[20px]" type="text" />
+              <input
+                name="name"
+                className="p-2 bg-[#fbfbfb] rounded-[20px]"
+                type="text"
+              />
               <label className="font-[600]">Email address</label>
-              <input className="p-2 bg-[#fbfbfb] rounded-[20px]" type="text" />
+              <input
+                name="email"
+                className="p-2 bg-[#fbfbfb] rounded-[20px]"
+                type="text"
+              />
               <label className="font-[600]">Write your problem here </label>
-              <textarea rows={"7"} className=" bg-[#fbfbfb] p-2 rounded-[20px]" type="text" />
+              <textarea
+                rows={"7"}
+                name="message"
+                className=" bg-[#fbfbfb] p-2 rounded-[20px]"
+                type="text"
+              />
+              <button
+                type="submit"
+                className="bg-[#383838] text-white px-10 py-2 mt-4 rounded-[20px]"
+              >
+                Submit
+              </button>
             </form>
-            <button className="bg-[#383838] text-white px-10 py-2 mt-4 rounded-[20px]">Submit</button>
           </div>
         </div>
       </MainLayout>
