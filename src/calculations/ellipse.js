4d1ed8093b6a4fac7d936e9430e88d2392717539
abdul-ahad12 <abdulkouzer@gmail.com>
2022-11-06 00:18:17 +0530

    ellipse

--- src/calculations/ellipse.js
+++ src/calculations/ellipse.js
@@ -16,83 +16,140 @@ class Ellipse {
     let k = o / (-2 * m);
 
     let c, e, length_major_axis, length_minor_axis, length_of_latus_rectum;
-    if(a>b)
-    {
-	 c = Math.sqrt((a*a) - (b-b));
-		 e = c/b;	
-	 length_major_axis = 2*a;
-		length_minor_axis = 2*b;
-		length_of_latus_rectum = (2*b*b)/a;
+    if (a > b) {
+      c = Math.sqrt(a * a - (b - b));
+      e = c / b;
+      length_major_axis = 2 * a;
+      length_minor_axis = 2 * b;
+      length_of_latus_rectum = (2 * b * b) / a;
+    } else if (b > a) {
+      c = Math.sqrt(b * b - (a - a));
+      e = c / b;
+      length_major_axis = 2 * b;
+      length_minor_axis = 2 * a;
+      length_of_latus_rectum = (2 * a * a) / b;
     }
-	else if(b>a)
-    {
-		c = Math.sqrt((b*b) - (a-a));
-		e = c/b;
-		length_major_axis = 2*b;
-		length_minor_axis = 2*a;
-		length_of_latus_rectum = (2*a*a)/b;
-    }
-	if(h===0 && k===0){
-		if(a>b){
-		let vertex = a;
-		let focus = a*e;
-		let eqn_directrix = a/e;
-		let	eqn_ll = a*e;
-       
+    if (h === 0 && k === 0) {
+      if (a > b) {
+        let vertex = a;
+        let focus = a * e;
+        let eqn_directrix = a / e;
+        let eqn_ll = a * e;
+        let eqnMajor = `y=0`;
+        let eqnMinor = `x-0`;
 
-        return [a,b,h,k,e,vertex,focus,length_major_axis,length_minor_axis,eqn_ll,length_of_latus_rectum,eqn_directrix]
-        }
-            
-			
-			// return 'a = '+a+'b = '+b+';  Centre = ('+h+','+k+');  eccentricity = '+e+';  Vertices = (±'+vertex+',0);  focii = (±'+focus+',0);  eqn of major axis: y = 0'; 
-            //  Length of major axis = '+length_major_axis+';  Eqn of minor axis: x = 0';  Length of minor axis= '+length_minor_axis+';  Eqn of Latus Rectum: x = ±'+eqn_ll+';  Length of Latus rectum = '+length_of_latus_rectum+';  Eqn of directrix: x = ±'+eqn_directrix;
-		else if(a<b)
-        {
-		let	vertex = b;
-		let	focus = b*e;
-		let	eqn_directrix = b/e;
-		let	eqn_ll = b*e;
-            return [a,b,h,k,e,vertex,focus,length_major_axis,length_minor_axis,eqn_ll,length_of_latus_rectum,eqn_directrix]
-        }
-			
-			// return 'a = '+a+'b = '+b+';  Centre = ('+h+','+k+');  eccentricity = '+e+';  Vertices = (±'+vertex+',0);  focii = (±'+focus+',0);  eqn of major axis: x = 0';  Length of major axis = '+length_major_axis+';  Eqn of minor axis: y = 0';  Length of minor axis= '+length_minor_axis+';  Eqn of Latus Rectum: x = ±'+eqn_ll+';  Length of Latus rectum = '+length_of_latus_rectum+';  Eqn of directrix: x = ±'+eqn_directrix;
-		
+        return [
+          a,
+          b,
+          h,
+          k,
+          e,
+          vertex,
+          focus,
+          eqnMajor,
+          length_major_axis,
+          eqnMinor,
+          length_minor_axis,
+          eqn_ll,
+          length_of_latus_rectum,
+          eqn_directrix,
+        ];
+      }
+
+      // return 'a = '+a+'b = '+b+';  Centre = ('+h+','+k+');  eccentricity = '+e+';  Vertices = (±'+vertex+',0);  focii = (±'+focus+',0);  eqn of major axis: y = 0';
+      //  Length of major axis = '+length_major_axis+';  Eqn of minor axis: x = 0';  Length of minor axis= '+length_minor_axis+';  Eqn of Latus Rectum: x = ±'+eqn_ll+';  Length of Latus rectum = '+length_of_latus_rectum+';  Eqn of directrix: x = ±'+eqn_directrix;
+      else if (a < b) {
+        let vertex = b;
+        let focus = b * e;
+        let eqn_directrix = b / e;
+        let eqn_ll = b * e;
+		let eqnMajor = `x=0`;
+        let eqnMinor = `y-0`;
+        return [
+			a,
+			b,
+			h,
+			k,
+			e,
+			vertex,
+			focus,
+			eqnMajor,
+			length_major_axis,
+			eqnMinor,
+			length_minor_axis,
+			eqn_ll,
+			length_of_latus_rectum,
+			eqn_directrix,
+		  ];
+      }
+
+      // return 'a = '+a+'b = '+b+';  Centre = ('+h+','+k+');  eccentricity = '+e+';  Vertices = (±'+vertex+',0);  focii = (±'+focus+',0);  eqn of major axis: x = 0';  Length of major axis = '+length_major_axis+';  Eqn of minor axis: y = 0';  Length of minor axis= '+length_minor_axis+';  Eqn of Latus Rectum: x = ±'+eqn_ll+';  Length of Latus rectum = '+length_of_latus_rectum+';  Eqn of directrix: x = ±'+eqn_directrix;
+    } else if (h !== 0 && k !== 0) {
+      if (a > b) {
+		let vertex=0;
+        let focus = a * e;
+        let eqn_directrix = a / e;
+        let eqn_ll = a * e;
+		let eqnMajor = `y-${k}=0`;
+        let eqnMinor = `x-${h}=0`;
+
+        return [
+			a,
+			b,
+			h,
+			k,
+			e,
+			vertex,
+			focus,
+			eqnMajor,
+			length_major_axis,
+			eqnMinor,
+			length_minor_axis,
+			eqn_ll,
+			length_of_latus_rectum,
+			eqn_directrix,
+		  ];
+      }
+
+      // return 'a = '+a+'b = '+b+';  Centre = ('+h+','+k+');  eccentricity = '+e+';  Vertices = ('+h+'±'+a+','+k+');  focii = ('+h+'±'+focus+','+k+');  eqn of major axis: y-'+k+' = 0;  Length of major axis = '+length_major_axis+';  Eqn of minor axis: x - '+h+' = 0;  Length of minor axis= '+length_minor_axis+';  Eqn of Latus Rectum: x ='+h+'±'+eqn_ll+';  Length of Latus rectum = '+length_of_latus_rectum+';  Eqn of directrix: x ='+h+'±'+eqn_directrix;
+      else if (a < b) {
+		let vertex=0
+        let focus = b * e;
+        let eqn_directrix = b / e;
+        let eqn_ll = b * e;
+		let eqnMajor = `x-${h}=0`;
+        let eqnMinor = `y-${k}=0`;
+
+        return[
+			a,
+			b,
+			h,
+			k,
+			e,
+			vertex,
+			focus,
+			eqnMajor,
+			length_major_axis,
+			eqnMinor,
+			length_minor_axis,
+			eqn_ll,
+			length_of_latus_rectum,
+			eqn_directrix,
+		  ];
+      }
     }
-	else if(h!==0 && k!==0)
-    {
-		if(a>b)
-        {
-			let focus = a*e;
-			let eqn_directrix = a/e;
-			let eqn_ll = a*e;
-			
-            return [a,b,h,k,e,focus,length_major_axis,length_minor_axis,eqn_ll,length_of_latus_rectum,eqn_directrix]
-        }
-			
-			// return 'a = '+a+'b = '+b+';  Centre = ('+h+','+k+');  eccentricity = '+e+';  Vertices = ('+h+'±'+a+','+k+');  focii = ('+h+'±'+focus+','+k+');  eqn of major axis: y-'+k+' = 0;  Length of major axis = '+length_major_axis+';  Eqn of minor axis: x - '+h+' = 0;  Length of minor axis= '+length_minor_axis+';  Eqn of Latus Rectum: x ='+h+'±'+eqn_ll+';  Length of Latus rectum = '+length_of_latus_rectum+';  Eqn of directrix: x ='+h+'±'+eqn_directrix;
-		else if(a<b){
-		let	focus = b*e;
-		let	eqn_directrix = b/e;
-		let	eqn_ll = b*e;
-	
-        return [a,b,h,k,e,focus,length_major_axis,length_minor_axis,eqn_ll,length_of_latus_rectum,eqn_directrix]
-			
-    }
-}
   }
 
-  areacircum(l,m)
-{
-	let a = Math.sqrt(m);
-	let b = Math.sqrt(l);
+  areacircum(l, m) {
+    let a = Math.sqrt(m);
+    let b = Math.sqrt(l);
 
-	let area = b;
+    let area = Math.PI * a * a;
 
-	let perimeter = 2*Math.PI*(Math.sqrt(((a*a)+(b*b))/2));
+    let perimeter = 2 * Math.PI * Math.sqrt((a * a + b * b) / 2);
 
-  return [area.toFixed(2), perimeter.toFixed(2)];
-}
-
+    return [area.toFixed(2), perimeter.toFixed(2)];
+  }
 }
 
 export default Ellipse;
