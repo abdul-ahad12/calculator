487e6e2b780381518afac57cd3c041a9be876b53
abdul-ahad12 <abdulkouzer@gmail.com>
2022-11-06 17:43:14 +0530

    hello

--- src/component/sections/resultTemp.js
+++ src/component/sections/resultTemp.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-const ResultTemp = ({ a, b, c, d, e, f, g, h, i, j, k }) => {
+const ResultTemp = ({ a, b, c, d, e, f, g, h, i, j, k ,subtitle1,subtitle2}) => {
   return (
     <div  className="border base:text-[15px] max-w-[1800px] md:text-[25px] lg:text-[30px] lg:p-5 base:p-2 border-black flex flex-col">
       <div className="text-start" >
         {" "}
         <div className="flex flex-row">
-        <div className="result">Length of Major axis</div>
+        <div className="result">Length of {subtitle1}</div>
         <div className="result">{a}</div>
         </div>
         <div className="flex flex-row">
 
-        <div className="result">Length of Minor axis</div>
+        <div className="result">Length of {subtitle2}</div>
         <div className="result">{b}</div>
         </div>
 
@@ -54,13 +54,13 @@ const ResultTemp = ({ a, b, c, d, e, f, g, h, i, j, k }) => {
 
         <div className="flex flex-row">
 
-        <div className="result">Equation of Major axis</div>
+        <div className="result">Equation of {subtitle1}</div>
         <div className="result">{i}</div>
         </div>
 
         <div className="flex flex-row">
 
-        <div className="result">Equation of Minor axis</div>
+        <div className="result">Equation of {subtitle2}</div>
         <div className="result">{j}</div>
         </div>
 
--- src/pages/hyperbola.js
+++ src/pages/hyperbola.js
@@ -4,36 +4,11 @@ import GeoTemp from '../component/sections/geoTemp'
 const Hyperbola = () => {
     const lineElements = [
         {
-          title: "Distance between two points",
+          title: "Attributes of Hyperbola",
     
-          link: "/distance",
+          link: "/attributesel",
         },
-        {
-          title: "Slope of a line",
-          link: "/slopeLine",
-        },
-        {
-          title: "Point of intersection of two lines",
-          link: "/pointofint",
-        },
-        {
-          title: "x-intercept and y-intercept of a line.",
-          link: "/xyintercept",
-        },
-        {
-          title: "Equation of perpendicular bisector.",
-          link: "/perpendicular",
-        },
-        {
-          title: "Equation of a line. Two point form.",
-    
-          link: "/twopointform",
-        },
-        {
-          title: "Equation of a line. Point slope form.",
-          link: "/pointslopeform",
-        },
-     
+         
       ];
   return (
     <div>
