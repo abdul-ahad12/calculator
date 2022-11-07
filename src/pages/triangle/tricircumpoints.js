f048bdac818921909ec9ddbb0d50d2e115e10a60
SufyanKhan0 <67229679+SufyanKhan0@users.noreply.github.com>
2022-11-07 17:33:45 +0530

    triangle ops

--- src/pages/triangle/tricircumpoints.js
+++ src/pages/triangle/tricircumpoints.js
@@ -4,84 +4,95 @@ import { MathComponent } from "mathjax-react";
 import Triangle from "../../calculations/triangle";
 
 const Tricircumpoints = () => {
-    const [coeficient, setcoeficient] = useState({
-        a: "",
-        b: "",
-        c: "",
-        d: "",
-        e: "",
-        f: "",
-      });
-      const [result, setResult] = useState("");
-    
-      const triangle = new Triangle(coeficient.a, coeficient.b, coeficient.c, coeficient.d, coeficient.e, coeficient.f);
-      console.log(coeficient);
-    
-      const onAChange = (e) => {
-        setcoeficient({
-          ...coeficient,
-          a: e.target.value,
-        });
-      };
-      const onBChange = (e) => {
-        setcoeficient({
-          ...coeficient,
-          b: e.target.value,
-        });
-      };
-      const onCChange = (e) => {
-        setcoeficient({
-          ...coeficient,
-          c: e.target.value,
-        });
-      };
-      const onDChange = (e) => {
-        setcoeficient({
-          ...coeficient,
-          d: e.target.value,
-        });
-      };
-      const onEChange = (e) => {
-        setcoeficient({
-          ...coeficient,
-          e: e.target.value,
-        });
-      };
-      const onFChange = (e) => {
-        setcoeficient({
-          ...coeficient,
-          f: e.target.value,
-        });
-      };
-    
-      const resultDumm = () => {
-        const result = triangle.tricircumpoints(coeficient.a, coeficient.b, coeficient.c,coeficient.d, coeficient.e, coeficient.f);
-        let x = result[0];
-        let y = result[1];
-        let c = `(${x},${y})`;
-        let e = (
-          <MathComponent tex={`Circumcentre :${c}`} />
-        );
-        setResult(e);
-      };
-    
-      const onResult = () => {
-        coeficient.aa === "" || coeficient.ba === "" || coeficient.ca === ""
-          ? alert("Enter all inputs")
-          : resultDumm();
-        // setvariables({
-        //     ax:"",
-        //     ay:"",
-        //     bx:"",
-        //     by:"",
-    
-        // })
-      };
-  
-  
-    return (
+  const [coeficient, setcoeficient] = useState({
+    a: "",
+    b: "",
+    c: "",
+    d: "",
+    e: "",
+    f: "",
+  });
+  const [result, setResult] = useState("");
+
+  const triangle = new Triangle(
+    coeficient.a,
+    coeficient.b,
+    coeficient.c,
+    coeficient.d,
+    coeficient.e,
+    coeficient.f
+  );
+  console.log(coeficient);
+
+  const onAChange = (e) => {
+    setcoeficient({
+      ...coeficient,
+      a: e.target.value,
+    });
+  };
+  const onBChange = (e) => {
+    setcoeficient({
+      ...coeficient,
+      b: e.target.value,
+    });
+  };
+  const onCChange = (e) => {
+    setcoeficient({
+      ...coeficient,
+      c: e.target.value,
+    });
+  };
+  const onDChange = (e) => {
+    setcoeficient({
+      ...coeficient,
+      d: e.target.value,
+    });
+  };
+  const onEChange = (e) => {
+    setcoeficient({
+      ...coeficient,
+      e: e.target.value,
+    });
+  };
+  const onFChange = (e) => {
+    setcoeficient({
+      ...coeficient,
+      f: e.target.value,
+    });
+  };
+
+  const resultDumm = () => {
+    const result = triangle.tricircumpoints(
+      coeficient.a,
+      coeficient.b,
+      coeficient.c,
+      coeficient.d,
+      coeficient.e,
+      coeficient.f
+    );
+    let x = result[0];
+    let y = result[1];
+    let c = `(${x},${y})`;
+    let e = <MathComponent tex={`Circumcentre :${c}`} />;
+    setResult(e);
+  };
+
+  const onResult = () => {
+    coeficient.aa === "" || coeficient.ba === "" || coeficient.ca === ""
+      ? alert("Enter all inputs")
+      : resultDumm();
+    // setvariables({
+    //     ax:"",
+    //     ay:"",
+    //     bx:"",
+    //     by:"",
+
+    // })
+  };
+
+  return (
     <div>
-         <TitleTemplate
+      <TitleTemplate
         title={"Circumcentre of a triangle from Vertices"}
         // formula={f1}
         type={"threePoints"}
@@ -101,6 +112,6 @@ const Tricircumpoints = () => {
         result={result}
       />
     </div>
-  )
-}
+  );
+};
 export default Tricircumpoints;
