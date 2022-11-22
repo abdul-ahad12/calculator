f7b449d22ef468ff49acccc9e9648758b87ab39b
SufyanKhan0 <67229679+SufyanKhan0@users.noreply.github.com>
2022-11-23 03:22:46 +0530

    privacy

--- src/pages/aboutus.js
+++ src/pages/aboutus.js
@@ -1,13 +1,20 @@
 import React from 'react'
-import Simple from '../component/sections/simple'
 import MainLayout from './mainLayout'
 
 const Aboutus = () => {
   return (
     <div>
-      <MainLayout>
-        <Simple title={"About us"} />
-      </MainLayout>
+    <MainLayout>
+    <div className='className="w-full flex mx-10 flex-col items-center justify-center h-[100%] '>
+      <div className="font-[500] base:mb-4 lg:mb-[4rem] base:text-[2rem] lg:text-[3rem] mt-7 ">
+        About Us
+      </div>
+      <div className="text-[2rem] lg:w-[60%]  ">
+      
+      </div>
+    </div>
+    </MainLayout>
+      
     </div>
   )
 }
